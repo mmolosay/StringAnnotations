cb95f430711ebe06d055f5f1c5c73713f047dd7c
mmolosay <mmolosays@gmail.com>
2022-08-06 20:52:30 +0300

    Change 'GROUP_ID'

--- buildSrc/src/main/java/Project.kt
+++ buildSrc/src/main/java/Project.kt
@@ -1,7 +1,7 @@
 object Project {
 
     const val APPLICATION_ID = "com.mmolosay.stringannotations"
-    const val GROUP_ID = "com.github.mmolosay"
+    const val GROUP_ID = "com.mmolosay.stringannotations"
     const val ARTIFACT_ID = "stringannotations" // repo name
     const val VERSION = "1.0.0"
 }
