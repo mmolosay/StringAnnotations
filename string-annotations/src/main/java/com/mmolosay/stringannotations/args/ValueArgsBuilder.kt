fb59eeb9ce32a150a0df086fa9b1a766818577ab
mmolosay <mmolosays@gmail.com>
2022-09-07 12:27:37 +0300

    Add methods with vararg parameters

--- string-annotations/src/main/java/com/mmolosay/stringannotations/args/ValueArgsBuilder.kt
+++ string-annotations/src/main/java/com/mmolosay/stringannotations/args/ValueArgsBuilder.kt
@@ -36,6 +36,11 @@ public interface ValueArgsBuilder {
     public fun color(producer: () -> Int): ValueArgs
 
     /**
+     * Adds specified [items] in [ValueArgs.colors] list.
+     */
+    public fun colors(vararg items: Int): ValueArgs
+
+    /**
      * Executes [block], scoped to [Adder] of [ValueArgs.colors] list.
      */
     public fun colors(block: Adder<Int>.() -> Unit): ValueArgs
@@ -53,6 +58,11 @@ public interface ValueArgsBuilder {
      * Executes [producer] and adds resulting clickable span to [ValueArgs.clickables] list.
      */
     public fun clickable(producer: () -> ClickableSpan): ValueArgs
+
+    /**
+     * Adds specified [items] in [ValueArgs.clickables] list.
+     */
+    public fun clickables(vararg items: ClickableSpan): ValueArgs
 
     /**
      * Executes [block], scoped to [Adder] of [ValueArgs.clickables] list.
@@ -74,6 +84,11 @@ public interface ValueArgsBuilder {
     public fun typefaceStyle(producer: () -> Int): ValueArgs
 
     /**
+     * Adds specified [items] in [ValueArgs.typefaceStyles] list.
+     */
+    public fun typefaceStyles(vararg items: Int): ValueArgs
+
+    /**
      * Executes [block], scoped to [Adder] of [ValueArgs.typefaceStyles] list.
      */
     public fun typefaceStyles(block: Adder<Int>.() -> Unit): ValueArgs
@@ -91,6 +106,11 @@ public interface ValueArgsBuilder {
      * Executes [producer] and adds resulting pixel size to [ValueArgs.absSizes] list.
      */
     public fun absoluteSize(producer: () -> Int): ValueArgs
+
+    /**
+     * Adds specified [items] in [ValueArgs.absSizes] list.
+     */
+    public fun absoluteSizes(vararg items: Int): ValueArgs
 
     /**
      * Executes [block], scoped to [Adder] of [ValueArgs.absSizes] list.
