53a4bdc3643bac4b2d802f717b835ce7bae6a23a
mmolosay <mmolosays@gmail.com>
2022-09-14 23:37:29 +0300

    Update version

--- buildSrc/src/main/java/Project.kt
+++ buildSrc/src/main/java/Project.kt
@@ -3,5 +3,5 @@ object Project {
     const val APPLICATION_ID = "com.mmolosay.stringannotations"
     const val GROUP_ID = "com.mmolosay.stringannotations"
     const val ARTIFACT_ID = "stringannotations" // repo name
-    const val VERSION = "1.5.0" // TODO: update
+    const val VERSION = "1.6.0" // TODO: update
 }
