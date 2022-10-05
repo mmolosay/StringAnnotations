77e31b58d1e332f2af9f9607bbff976750af6f8a
mmolosay <mmolosays@gmail.com>
2022-10-05 14:54:56 +0300

    Show demo3

--- sample-compose/src/main/java/com/mmolosay/sample/compose/MainScreen.kt
+++ sample-compose/src/main/java/com/mmolosay/sample/compose/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.mmolosay.sample.compose
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,11 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mmolosay.stringannotations.args.Arguments
 import com.mmolosay.stringannotations.compose.annotatedStringResource
+import com.mmolosay.stringannotations.spans.clickable.ClickableSpan
 
 // region Previews
 
@@ -48,6 +51,7 @@ fun Main() {
     ) {
         Demo1()
         Demo2()
+        Demo3()
     }
 }
 
@@ -71,6 +75,28 @@ private fun Demo2() {
     val formatValue1 = stringResource(R.string.value1)
     Text(
         text = annotatedStringResource(R.string.demo2, args, formatValue1)
+    )
+}
+
+@Composable
+private fun Demo3() {
+    val context = LocalContext.current
+    val clickable1 = ClickableSpan {
+        Toast
+            .makeText(context, "Clicked annotation with index=0", Toast.LENGTH_SHORT)
+            .show()
+    }
+    val appearance2 = ClickableSpan.Appearance(underlineText = true)
+    val clickable2 = ClickableSpan(appearance2) {
+        Toast
+            .makeText(context, "Clicked annotation with index=1", Toast.LENGTH_SHORT)
+            .show()
+    }
+    val args = Arguments {
+        clickables(clickable1, clickable2)
+    }
+    Text(
+        text = annotatedStringResource(R.string.demo3, args)
     )
 }
 
