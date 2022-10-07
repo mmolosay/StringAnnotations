b3f07bcc97ce78be7a717411075513c1196ce23e
mmolosay <mmolosays@gmail.com>
2022-10-07 14:32:04 +0300

    Move 'MutableCustomValues' into 'CustomAnnotationValuesBuilder'

--- sample-compose/src/main/java/com/mmolosay/sample/compose/custom/CustomAnnotationValuesBuilder.kt
+++ sample-compose/src/main/java/com/mmolosay/sample/compose/custom/CustomAnnotationValuesBuilder.kt
@@ -1,10 +1,11 @@
 package com.mmolosay.sample.compose.custom
 
+import com.mmolosay.stringannotations.args.qualified.MutableQualifiedList
 import com.mmolosay.stringannotations.compose.ComposeAnnotationValues
 
 class CustomAnnotationValuesBuilder(values: ComposeAnnotationValues) {
 
-    private val values = MutableCustomValues(values)
+    private val values = MutableCustomAnnotationValues(values)
 
     fun build(): CustomAnnotationValues =
         values
@@ -16,4 +17,9 @@ class CustomAnnotationValuesBuilder(values: ComposeAnnotationValues) {
     fun customs(vararg items: String) {
         values.customs.addAll(items)
     }
+
+    private class MutableCustomAnnotationValues(
+        values: ComposeAnnotationValues,
+        override val customs: MutableQualifiedList<String> = MutableQualifiedList("custom")
+    ) : CustomAnnotationValues, ComposeAnnotationValues by values
 }
--- sample-compose/src/main/java/com/mmolosay/sample/compose/custom/MutableCustomArguments.kt
+++
@@ -1,9 +0,0 @@
-package com.mmolosay.sample.compose.custom
-
-import com.mmolosay.stringannotations.args.qualified.MutableQualifiedList
-import com.mmolosay.stringannotations.compose.ComposeAnnotationValues
-
-class MutableCustomValues(
-    values: ComposeAnnotationValues,
-    override val customs: MutableQualifiedList<String> = MutableQualifiedList("custom")
-) : CustomAnnotationValues, ComposeAnnotationValues by values
