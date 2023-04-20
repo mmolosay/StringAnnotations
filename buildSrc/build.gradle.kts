2195b38b43d4405ce566056426592023645e1bfd
mmolosay <mmolosays@gmail.com>
2023-04-20 20:51:26 +0200

    Migrate to AGP 8.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ repositories {
 dependencies {
     // <1> define dependencies here using Maven coordinates, not plugin ID
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20") // koltin
-    implementation("com.android.tools.build:gradle:7.3.0") // com.android.library and stuff
+    implementation("com.android.tools.build:gradle:8.0.0") // com.android.library and stuff
 }
