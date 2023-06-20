19b6785ba4bb5bc80da6a408672da92be18a0733
mmolosay <mmolosays@gmail.com>
2023-06-20 13:50:48 +0200

    Upgrate AGP to '8.0.2'

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ group = "io.github.mmolosay"
 version = "1.0.0"
 
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.20" apply false
 }
