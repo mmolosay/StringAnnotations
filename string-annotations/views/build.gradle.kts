a6b14459864c2da632752da9b9b369ec82646a6d
mmolosay <mmolosays@gmail.com>
2023-06-24 16:41:55 +0200

    Update fragments dependency

--- string-annotations/views/build.gradle.kts
+++ string-annotations/views/build.gradle.kts
@@ -31,5 +31,5 @@ dependencies {
     api(project(":string-annotations:common:shared"))
     implementation(project(":string-annotations:common:internal"))
 
-    implementation("androidx.fragment:fragment-ktx:1.5.4") // for fragment extensions
+    implementation("androidx.fragment:fragment-ktx:1.6.0") // for fragment extensions
 }
